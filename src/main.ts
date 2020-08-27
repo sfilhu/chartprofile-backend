9b48a3b0cf7088c32432a4003a5f6e6c22b92593
sfilhu <sfilhu@gmail.com>
2020-08-26 23:31:25 -0300

    Add Procfile

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors    from 'cors'
+import morgan  from "morgan"
+import routes from "./routes"
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000);
+
---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+// import usersController from './controllers/UserController'
+const routes = express.Router();
+
+routes.get("/", async (request, response) => {
+  return response.json({ status: 200, msg: 'Deu tudo certo no deploy!' })
+});
+// routes.post("/users", usersController.post);
+// routes.post("/filterRegistred", usersController.filter);
+// routes.delete("/deleteUser/:id", usersController.delete);
+
+export default routes;
