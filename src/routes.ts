e24254cbf193a6667d5410e75cb8b752d5c4c322
sfilhu <sfilhu@gmail.com>
2020-08-26 23:44:35 -0300

    add git remote heroku

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 // import usersController from './controllers/UserController'
 const routes = express.Router();
 
-routes.get("/users", async (request, response) => {
+routes.get("/", async (request, response) => {
   return response.json({ status: 200, msg: 'Deu tudo certo no deploy!' })
 });
 // routes.post("/users", usersController.post);
