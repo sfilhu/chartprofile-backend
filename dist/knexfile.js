966914c05678025fc947525298238f60b6bb2e75
sfilhu <sfilhu@gmail.com>
2020-08-28 19:14:50 -0300

    build project

---
+++ dist/knexfile.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var path_1 = __importDefault(require("path"));
+exports.default = {
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: path_1.default.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        },
+        migrations: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
+        },
+        useNullAsDefault: true
+    },
+    production: {
+        client: 'pg',
+        connection: {
+            host: process.env.DATABASE_URL,
+            database: 'fusion_db',
+            user: 'postgres',
+            password: 'asdwq10tt'
+        },
+        migrations: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
+        }
+    }
+};
+// postgres://pdxiacoaotpcyp:7c1afd517c29402179072b65dac07b0e890da65c34c7d98a0192dae6550a2d0f@ec2-54-86-57-171.compute-1.amazonaws.com:5432/dcqot9jjgs73eh
--- dist/src/main.js
+++ dist/src/main.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var morgan_1 = __importDefault(require("morgan"));
@@ -12,4 +13,5 @@ app.use(cors_1.default());
 app.use(morgan_1.default("dev"));
 app.use(express_1.default.json());
 app.use(routes_1.default);
+dotenv_1.default.config();
 app.listen(process.env.PORT || 3000);
