d7b88732ce3ceb8569f90f90bd61a86498cefdee
sfilhu <sfilhu@gmail.com>
2020-09-01 18:24:25 -0300

    adjust knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -31,13 +31,15 @@ export default {
   },
   production: {
     client: 'pg',
-    connection: {
-      // url : process.env.DATABASE_URL,
-      host : 'https://ec2-54-86-57-171.compute-1.amazonaws.com:5432',
-      database: 'dcqot9jjgs73eh',
-      user: 'pdxiacoaotpcyp',
-      password: '7c1afd517c29402179072b65dac07b0e890da65c34c7d98a0192dae6550a2d0f'
-    },
+    connection: 'postgres://pdxiacoaotpcyp:7c1afd517c29402179072b65dac07b0e890da65c34c7d98a0192dae6550a2d0f@ec2-54-86-57-171.compute-1.amazonaws.com:5432/dcqot9jjgs73eh',
+    // {
+    //   // url : process.env.DATABASE_URL,
+    //   host : 'ec2-54-86-57-171.compute-1.amazonaws.com',
+    //   port: '5432',
+    //   database: 'dcqot9jjgs73eh',
+    //   user: 'pdxiacoaotpcyp',
+    //   password: '7c1afd517c29402179072b65dac07b0e890da65c34c7d98a0192dae6550a2d0f'
+    // },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
